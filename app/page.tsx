1773dd6b87bf4a86c3773077d5afe9ad9f5537c2
lupinthethirdgentleman <lupinthethirdgentleman@gmail.com>
2024-02-07 08:54:00 +0000

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,97 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
+import { Button, Card, Flex, Layout } from "antd";
+import {
+  AlignLeftOutlined,
+  DownloadOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  WifiOutlined,
+} from "@ant-design/icons";
 
-export default function Home() {
+import { Content, Header } from "antd/es/layout/layout";
+import Title from "antd/es/typography/Title";
+// Could not find the module "E:\projects\test\bi_custom\node_modules\antd\es\index.js
+
+import BarChart from "./components/BarChart";
+import DonutChart from "./components/DonutChart";
+
+import { covidDataService } from "./services/covidDataService";
+
+const headerStyle: React.CSSProperties = {
+  backgroundColor: "#fff",
+  display: "flex",
+  alignItems: "center",
+};
+
+const contentStyle: React.CSSProperties = {
+  padding: "0 64px 0 64px",
+};
+
+const footerStyle: React.CSSProperties = {
+  padding: "24px",
+  justifyContent: "space-between",
+};
+
+const buttonGroupStyle: React.CSSProperties = {
+  margin: "24px 0",
+};
+
+const cardWrapperStyle: React.CSSProperties = {
+  gap: "24px",
+};
+
+const cardStyle: React.CSSProperties = {
+  flex: "1",
+};
+
+export default async function Home() {
+  const chartData = await covidDataService();
+
+  if (!chartData) {
+    return <div>Data not found</div>;
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Layout>
+      <Header style={headerStyle}>
+        <Title level={3}>COVID Report</Title>
+      </Header>
+      <Content style={contentStyle}>
+        <Flex justify="space-between" align="center">
+          <Title level={4}>Dashboard</Title>
+          <Flex gap="middle" style={buttonGroupStyle}>
+            <Button icon={<DownloadOutlined />}>Export to PDF</Button>
+            <Button icon={<AlignLeftOutlined />}>Notes (3)</Button>
+            <Button icon={<WifiOutlined />}>Filter 9+</Button>
+          </Flex>
+        </Flex>
+        <Flex style={cardWrapperStyle}>
+          <Card
+            title="Bar Chart"
+            style={cardStyle}
+            actions={[
+              <Flex key="left-card" style={footerStyle}>
+                <HeartOutlined />
+                <MessageOutlined />
+              </Flex>,
+            ]}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <BarChart covidStats={chartData.data} />
+          </Card>
+          <Card
+            title="Doughnut Chart"
+            style={cardStyle}
+            actions={[
+              <Flex key="right-card" style={footerStyle}>
+                <HeartOutlined />
+                <MessageOutlined />
+              </Flex>,
+            ]}
+          >
+            <DonutChart covidStats={chartData.data} />
+          </Card>
+        </Flex>
+      </Content>
+    </Layout>
   );
 }
